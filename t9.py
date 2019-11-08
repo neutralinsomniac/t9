a92ee014df969b89dc6dd028ecd652521ae5ae4f
Jeremy O'Brien <neutral@fastmail.com>
2019-11-07 21:10:42 -0500

    turn t9 engine into a class and add robust backspace support

--- t9.py
+++ t9.py
@@ -11,87 +11,122 @@ if len(sys.argv) != 2:
     print("usage: {} <dict.txt>".format(sys.argv[0]))
     sys.exit(1)
 
-t9 = {'.': 1, '!': 1, '?': 1,
-      ':': 1, '-': 1, "'": 1,
-      '(': 1, ')': 1, ';': 1,
-      'a': 2, 'b': 2, 'c': 2,
-      'd': 3, 'e': 3, 'f': 3,
-      'g': 4, 'h': 4, 'i': 4,
-      'j': 5, 'k': 5, 'l': 5,
-      'm': 6, 'n': 6, 'o': 6,
-      'p': 7, 'q': 7, 'r': 7, 's': 7,
-      't': 8, 'u': 8, 'v': 8,
-      'w': 9, 'x': 9, 'y': 9, 'z': 9}
+class T9Engine():
+    T9 = {'.': 1, '!': 1, '?': 1,
+          ':': 1, '-': 1, "'": 1,
+          '(': 1, ')': 1, ';': 1,
+          'a': 2, 'b': 2, 'c': 2,
+          'd': 3, 'e': 3, 'f': 3,
+          'g': 4, 'h': 4, 'i': 4,
+          'j': 5, 'k': 5, 'l': 5,
+          'm': 6, 'n': 6, 'o': 6,
+          'p': 7, 'q': 7, 'r': 7, 's': 7,
+          't': 8, 'u': 8, 'v': 8,
+          'w': 9, 'x': 9, 'y': 9, 'z': 9}
 
-lookup = {}
+    def __init__(self):
+        self._lookup = {}
+        self._cur = self._lookup
+        self._history = []
+        self._n = 0
+        self._ind = 0
 
-def print_candidates(tree, n, ind=0):
-    candidate = tree
-    while candidate:
-        if 'words' in candidate:
-            print(ERASE_LINE + "\r" + candidate['words'][ind % len(candidate['words'])][:n] + " " + str(candidate['words']), end='')
-            break
-        for key in iter(candidate):
-            if key != "words":
-                candidate = candidate[key]
-                break
+    def load_dict(self, filename):
+        with open(filename, 'r') as dictionary:
+            for word in dictionary.readlines():
+                cur = self._lookup
+                for c in word[:-1]:
+                    num = self.T9[c.lower()]
+                    if num not in cur:
+                        cur[num] = {}
+                    cur = cur[num]
+                if 'words' not in cur:
+                    cur['words'] = []
+                cur['words'].append(word[:-1])
+
+    def add_digit(self, digit):
+        if digit not in self._cur:
+            return False
+        self._n += 1
+        self._history.append(self._cur)
+        self._cur = self._cur[digit]
+        self._ind = 0
+        return self.get_completion()
+
+    def backspace(self):
+        self._ind = 0
+        if len(self._history) > 0:
+            self._cur = self._history.pop()
+            self._n -= 1
+            return self.get_completion()
+        else:
+            self._cur = self._lookup
+            return False
+
+    def get_completion(self):
+        candidate = self._cur
+        while candidate:
+            if 'words' in candidate:
+                return candidate['words'][self._ind % len(candidate['words'])][:self._n]
+            for key in iter(candidate):
+                if key != "words":
+                    candidate = candidate[key]
+                    break
+
+    def next_completion(self):
+        self._ind += 1
+        return self.get_completion()
+
+    def new_completion(self):
+        self._cur = self._lookup
+        self._history.clear()
+        self._ind = 0
+        self._n = 0
+
+    def get_cur_completion_len(self):
+        return self._n
     
-def load_dict(filename):
-    with open(sys.argv[1], "r") as dictionary:
-        for word in dictionary.readlines():
-            cur = lookup
-            for c in word[:-1]:
-                num = t9[c.lower()]
-                if num not in cur:
-                    cur[num] = {}
-                cur = cur[num]
-            if 'words' not in cur:
-                cur['words'] = []
-            cur['words'].append(word[:-1])
+
+t9_engine = T9Engine()
 
 print("load...")
 start = time.time()
-load_dict(sys.argv[1])
+t9_engine.load_dict(sys.argv[1])
 print("loaded in {}s".format(time.time() - start))
 
-cur = lookup
-n = 0
-history = []
+line = ''
+cur_word = ''
 
 exit = False
 while exit == False:
     key = getkey()
     if key in '123456789':
-        digit = int(key)
-        if digit not in cur:
-            continue
-        n += 1
-        print_candidates(cur[digit], n)
-        history.append(cur)
-        cur = cur[digit]
-        ind = 0
-        continue
-    if key == keys.TAB:
-        ind += 1
-        print_candidates(cur, n, ind)
-        continue
+        tmp = t9_engine.add_digit(int(key))
+        if tmp:
+            cur_word = tmp
+    elif key == keys.TAB:
+        cur_word = t9_engine.next_completion()
     elif key == '0' or key == keys.SPACE:
-        cur = lookup
-        n = 0
-        history.clear()
-        print("")
+        line += cur_word + ' '
+        cur_word = ''
+        t9_engine.new_completion()
     elif key == keys.BACKSPACE:
-        ind = 0
-        if len(history) > 0:
-            cur = history.pop()
-            n -= 1
+        if t9_engine.get_cur_completion_len() == 0:
+            if len(line) == 0:
+                continue
+            line = line[:-1]
+            words = line.split(' ')
+            cur_word = words[-1]
+            if len(cur_word) != 0:
+                line = line[:-1*len(cur_word)]
+            for c in cur_word:
+                t9_engine.add_digit(T9Engine.T9[c])
         else:
-            cur = lookup
-        if cur == lookup:
-            print(ERASE_LINE + "\r", end='')
-        else:
-            print_candidates(cur, n)
-        continue
+            tmp = t9_engine.backspace()
+            if tmp:
+                cur_word = tmp
+            else:
+                cur_word = ''
     elif key == 'q':
         exit = True
-        continue
+    print(ERASE_LINE + "\r" + line + cur_word, end='')
